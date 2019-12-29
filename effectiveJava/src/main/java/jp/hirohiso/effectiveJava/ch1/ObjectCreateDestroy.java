21a36dd9cb1155358d79f1346adf9d19ebe4b387
Irie HIronori <irie.h126@gmail.com>
2019-12-30 00:20:19 +0900

    Chapter1

--- effectiveJava/src/main/java/jp/hirohiso/effectiveJava/ch1/ObjectCreateDestroy.java
+++ effectiveJava/src/main/java/jp/hirohiso/effectiveJava/ch1/ObjectCreateDestroy.java
@@ -3,15 +3,20 @@ package jp.hirohiso.effectiveJava.ch1;
 public class ObjectCreateDestroy {
 
     public static void main(String[] args) {
-        // 項目１:コンストラクタの代わりに static ファクトリーメソッドを検討する
+        {
+            // 項目１:コンストラクタの代わりに static ファクトリーメソッドを検討する
 
-        //インスタンスを新たに生成する
-        Boolean a = new Boolean(false);
-        //インスタンスを新たに生成せずに、すでにあるものを使いまわす
-        Boolean b = Boolean.valueOf(false);
+            //インスタンスを新たに生成する
+            Boolean a = new Boolean(false);
+            //インスタンスを新たに生成せずに、すでにあるものを使いまわす
+            Boolean b = Boolean.valueOf(false);
+            //Integerも-127～ある一定の数までインスタンスを生成して、キャッシュをもってる。
+            Integer.valueOf(1);
 
-        //イミュータブルのため、使いまわしたほうがよい。
+            //イミュータブルのため、使いまわしたほうがよい。
+        }
 
     }
+
 
 }
